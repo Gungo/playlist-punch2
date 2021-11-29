eca41c17a472f43c139d872372c8916dba20e4ce
gungo <gungosis@edugenesis.com>
2021-11-29 03:06:22 -0500

    changed selected playlists to reflect all prev. selected - other minor changes

--- src/components/PlaylistCounter.js
+++ src/components/PlaylistCounter.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
 class PlaylistCounter extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <h2>{this.props.playlists.length} playlists</h2>
+        {(this.props.playlists.length == 1)
+          ? <h2>{this.props.playlists.length} playlist</h2>
+          : <h2>{this.props.playlists.length} playlists</h2>
+        }
       </div>
     );
   }
